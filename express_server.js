4fdc506a318790263bcf9dcd807fd21c5d24e1cd
Lauren Ashley <la.zaubi@gmail.com>
2022-10-24 10:36:43 -0700

    created obj to store user registrstion data

--- express_server.js
+++ express_server.js
@@ -10,6 +10,19 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+const users = {
+  userRandomID: {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: 'purple-monkey-dinosaur',
+  },
+  user2RandomID: {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: 'dishwasher-funk',
+  },
+};
+
 const generateRandomString = () => {
   const randomNum = Math.random().toString(20); // Specify radix, base to use for numeric vals
   return randomNum.substring(2, 8);
