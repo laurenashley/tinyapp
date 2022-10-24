f628c53c150bb55f18db0cbf99445e300f37cef3
Lauren Ashley <la.zaubi@gmail.com>
2022-10-23 21:05:59 -0700

    added edit feature to change longurl for given id

--- express_server.js
+++ express_server.js
@@ -31,6 +31,14 @@ app.post('/urls', (req, res) => {
   res.redirect(`/urls/${id}`);
 });
 
+app.post('/urls/:id/edit', (req, res) => {
+  console.log(req.body);
+  const id = req.params.id;
+  urlDatabase[id] = req.body.newURL;
+  console.log(urlDatabase);
+  res.redirect('/urls');
+});
+
 app.post('/urls/:id/delete', (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
