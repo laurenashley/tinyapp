02af68bfee3589634c0b3e72d6e81e19e8fcde3f
Lauren Ashley <la.zaubi@gmail.com>
2022-10-24 07:21:03 -0700

    added username cookie

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -19,14 +21,26 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+app.post('/login', (req, res) => {
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies);
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies);
+  console.log(req.body, res.cookie); // Log the POST request body to the console
+  const username = req.body.username;
+  res.cookie(username, username); // To Do overwrite existing cookie or allow multiple?
+  res.redirect('/urls');
+});
+
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  console.log('Cookies: ', req.cookies);
+  const templateVars = { urls: urlDatabase, username: req.cookies['username'] };
   res.render('urls_index', templateVars);
 });
 
 app.post('/urls', (req, res) => {
   const id = generateRandomString();
-  console.log(req.body); // Log the POST request body to the console
+  // console.log(req.body); // Log the POST request body to the console
   urlDatabase[id] = req.body.longURL;
   res.redirect(`/urls/${id}`);
 });
@@ -51,12 +65,13 @@ app.get('/u/:id', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = { username: req.cookies['username']};
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
   const myID = req.params.id;
-  const templateVars = { id: myID, longURL: urlDatabase[myID] };
+  const templateVars = { id: myID, longURL: urlDatabase[myID], username: req.cookies['username'] };
   res.render('urls_show', templateVars);
 });
 
