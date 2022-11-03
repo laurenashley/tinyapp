c23d8241ba05928dee36eb1e9c3d3d578cccbaa3
Lauren Ashley <la.zaubi@gmail.com>
2022-11-03 11:46:28 -0700

    update / redirects

--- server/express_server.js
+++ server/express_server.js
@@ -29,7 +29,7 @@ app.listen(PORT, () => {
 });
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  isLoggedIn(req) ? res.redirect('/urls') : res.redirect('/login');
 });
 
 app.get('/hello', (req, res) => {
